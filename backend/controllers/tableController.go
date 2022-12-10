1f1cc9feb2f8a7cddd91c6a0923800c9ff342a29
Dipu Mohanto <dipu.mohanto@selise.ch>
2022-12-10 11:17:35 +0600

    Order controller implemented.

--- backend/controllers/tableController.go
+++ backend/controllers/tableController.go
@@ -1,27 +1,149 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"fmt"
+	"golang-resturant-management/database"
+	"golang-resturant-management/models"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var tableCollection *mongo.Collection = database.OpenCollection(database.Client, "table")
 
 func GetTables() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
+		result, err := tableCollection.Find(ctx, bson.M{})
+
+		if err != nil {
+			msg := fmt.Sprintf("message: error occured while listing table items")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+
+		var allTables []bson.M
+
+		if err := result.All(ctx, allTables); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, allTables)
 	}
 }
 
 func GetTable() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
+		tableId := c.Param("table_id")
+		var table models.Table
+
+		err := tableCollection.FindOne(
+			ctx,
+			bson.M{"table_id": tableId},
+		).Decode(&table)
+
+		if err != nil {
+			msg := fmt.Sprintf("message: error occured while listing table item")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+
+		c.JSON(http.StatusOK, table)
 	}
 }
 
 func CreateTable() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
+		var table models.Table
+
+		if err := c.BindJSON(&table); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		if validationErr := validate.Struct(table); validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+
+		table.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		table.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		table.ID = primitive.NewObjectID()
+		table.Table_id = table.ID.Hex()
+
+		result, insertErr := tableCollection.InsertOne(
+			ctx,
+			table,
+		)
+
+		if insertErr != nil {
+			msg := fmt.Sprintf("Table item was not created")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+
+		c.JSON(http.StatusOK, result)
 	}
 }
 
 func UpdateTable() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+		var table models.Table
 
+		if err := c.BindJSON(&table); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		tableId := c.Param("table_id")
+		var updateObj primitive.D
+
+		if table.Number_of_guests != nil {
+			updateObj = append(updateObj, bson.E{"number_of_guests", table.Number_of_guests})
+		}
+		if table.Table_number != nil {
+			updateObj = append(updateObj, bson.E{"table_number", table.Table_number})
+		}
+		table.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		updateObj = append(updateObj, bson.E{"updated_at", table.Updated_at})
+
+		upsert := true
+		filter := bson.M{"table_id": tableId}
+		opt := options.UpdateOptions{
+			Upsert: &upsert,
+		}
+		result, err := tableCollection.UpdateOne(
+			ctx,
+			filter,
+			bson.D{
+				{"$set", updateObj},
+			},
+			&opt,
+		)
+
+		if err != nil {
+			msg := fmt.Sprintf("Table update failed")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+
+		c.JSON(http.StatusOK, result)
 	}
 }
